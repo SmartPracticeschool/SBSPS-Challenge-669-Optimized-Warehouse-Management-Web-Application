d8181cfacf8684c493dc3d1bd2f78744be8569b6
SamratChaudhuri <64581125+SamratChaudhuri@users.noreply.github.com>
2020-06-23 09:08:44 +0530

    Create pipeline_read_me.py

---
+++ Machine Learning Model/Pipeline Selection/pipeline_read_me.py
@@ -0,0 +1,3 @@
+Out of the 8 pipelines created using algorithms such as Random Forest Regressor Algorithm and Extra Trees Regressor Algorithm,
+Pipeline 3, stood out as the best pipeline and thereby was converted and adapted into later models for use, it has an accuracy 
+of 76.2%
